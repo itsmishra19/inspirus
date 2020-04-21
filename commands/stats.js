d14e31c3764d2004061efb3a272a35d81b644abb
HYDROXIN <40756526+inhydrox@users.noreply.github.com>
2020-04-21 12:12:22 +0530

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -16,6 +16,7 @@ thumbnail: {
 url: client.user.avatarURL 
  },
 description: `
+I am ${client.user.username} a cool discord bot, check my [repository](https://github.com/inhydrox/inspirus)
 • Created By: ${client.util.codeBlock(owner)}
 • Ping: ${client.util.codeBlock(wsPING+'ms')}
 • Uptime: ${client.util.codeBlock(client.util.timeParser(client.uptime))}
